772fd51635f1912aab19e69fa1a308e00a41a9e5
Lucas Scott <lucas.scott@rogers.com>
2020-04-12 17:43:19 -0400

    g
    g

--- blackjack.py
+++ blackjack.py
@@ -10,7 +10,12 @@ class Game(Tk):
     #Initialize frames for UI
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
+        app = Tk()
+
         self.geometry("800x700") # set window size
+
+        backpic = PhotoImage(file = "C:\\Images\\background.jpeg")
+        label1 = Label(self, image=backpic).pack()
 
 
 gametest = Game()
